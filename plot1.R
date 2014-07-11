5bdb300285ae46295bd9d47728542f8631b0ec3d
pwarawit <p.warawit@gmail.com>
2014-07-11 23:40:59 +0700

    updated plot1.R
    updated plot1.R

--- plot1.R
+++ plot1.R
@@ -4,10 +4,36 @@ plot1 <- function(){
     # class project on July 11, 2014
 
     # Check if the text file exist, if not - download the zip file and unzip it
+    zip_filename <- "exdata-data-household_power_consumption.zip"
+    txt_filename <- "household_power_consumption.txt"
+    if (!file.exists(txt_filename)){
+        # Text file does not exist - check if zip file exists
+        if (!file.exists(zip_filename)){
+            # print ("No zip file, need to both download and unzip")
+            fileurl <- "http://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+            download.file(fileurl,zip_filename)
+            unzip(zip_filename)
+        } else {
+            # print ("Zip file exists, but not text file, just need to unzip")
+            unzip(zip_filename)
+        }
+    } 
     
+    # Load data into table
+    full_data <- read.table(txt_filename, sep=";", header=TRUE)
+        
+    # Subset only data between 2007-02-01 and 2007-02-02 into data object
+    begin_date <- as.Date("2007-02-01")
+    end_date <- as.Date("2007-02-02")
+    data <- full_data[(as.Date(full_data$Date,format="%d/%m/%Y") <= end_date & as.Date(full_data$Date,format="%d/%m/%Y") >= begin_date),]
     
+    # Convert $Date into date value and $\
+    #newdata<-cbind(as.Date(data$Date, format="%d/%m/%Y"),data)
+    data$Date <- as.Date(data$Date, format="%d/%m/%Y")
+    #data
     
-    
-    
+    # This is the plot
+    hist(as.numeric(data$Global_active_power)/1000, main="Global Active Power", col="Red", xlab="Global Active Power (kilowatts)")
+    #data <- data[,c()]
     
 }    
