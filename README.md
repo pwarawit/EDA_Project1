63bcf04284fb93823435d4410e5a0e1a97bb51d9
pwarawit <p.warawit@gmail.com>
2014-07-11 18:51:28 +0700

    Initial version
    initial version

---
+++ README.md
@@ -0,0 +1,5 @@
+Exploratory Data Analysis - Project 1
+====
+
+This repo contains R codes and result plots created for the project 1 of Exploratory Data Analysis course. 
+
---
+++ plot1.R
@@ -0,0 +1,13 @@
+plot1 <- function(){
+    # plot1.R 
+    # written by PanaEk Warawit as part of Exploratory Data Analysis 
+    # class project on July 11, 2014
+
+    # Check if the text file exist, if not - download the zip file and unzip it
+    
+    
+    
+    
+    
+    
+}    
